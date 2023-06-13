76ecd06685b362a74fd68f32462e5f9127912bfc
Karine Moutier <karine0805@live.fr>
2023-06-13 16:14:39 +0200

    modificaion formulaire+ validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,8 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelector(".btnClose");
+const modalSucces = document.querySelector(".validation");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -21,7 +23,11 @@ function launchModal() {
 }
 
 //fermeture modal
+modalClose.addEventListener("click", close);
 
+function close() {
+  modalbg.style.display = "none";
+}
 
 
 //validation formulaire
@@ -30,20 +36,20 @@ function launchModal() {
 let validation = document.getElementById('envoi');
 let prenom = document.getElementById('first');
 let erreurPrenom = document.getElementById('erreurPrenom');
-let prenomValid = /^[a-zA-ZéèêîïÉÈÊôÔùçàÎÏÀ][a-zéèêîïùçêôà]+(-'\s[a-zA-ZéèîïÉÈÊôÔùçàÎÏÀ][a-zéèôêîïùçà]+)?/;
+let prenomValid = /^[a-zA-ZéèêîïÉÈÊôÔùçàÎÏÀ][A-Za-zéèêîïùçêôà]+(-'\s[a-zA-ZéèîïÉÈÊôÔùçàÎÏÀ][A-Za-zéèôêîïùçà]+)?/;
 let nom = document.getElementById('last');
 let erreurNom = document.getElementById('erreurNom');
-let nomValid = /^[a-zA-ZéèêîïÉÈÊôÔùçàÎÏÀ][a-zéèêîïùçêôà]+(-'\s[a-zA-ZéèîïÉÈÊôÔùçàÎÏÀ][a-zéèôêîïùçà]+)?/;
+let nomValid = /^[a-zA-ZéèêîïÉÈÊôÔùçàÎÏÀ][A-Za-zéèêîïùçêôà]+(-'\s[a-zA-ZéèîïÉÈÊôÔùçàÎÏÀ][A-Za-zéèôêîïùçà]+)?/;
 let email = document.getElementById('email');
 let erreurMail = document.getElementById('erreurMail');
-let emailValid = /^[a-zA-ZéèêîïÉÈÊôÔùçàÎÏÀ0-9][a-zéèêîïùçêôà0-9]+(-'\s[a-zA-ZéèîïÉÈÊôÔùçàÎÏÀ0-9][a-zéèôêîïùçà0-9]+)?+[@]+[a-z0-9]+[.]+[a-z0-9][a-z0-9][a-z0-9]+([a-z0-9])?/;
+let emailValid = /^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/;
 let birthdate = document.getElementById('birthdate');
 let erreurBirthdate = document.getElementById('erreurBirthdate');
-let birthdateValid = /^[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]/;
+let birthdateValid = /^[0-9]{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])/;
 let quantity = document.getElementById('quantity');//nombre de tournois précédents
 let erreurQuantity = document.getElementById('erreurQuantity');
 let quantityValid = /^[0-9]+([0-9]+)?/;
-let location = document.getElementsByName('location');//endroi ou veux participer le candidat
+let lieu = document.querySelectorAll("[name='location']");//endroit ou veux participer le candidat
 let erreurLocation = document.getElementById('erreurLocation');
 let checkbox1 = document.getElementById('checkbox1');//validation des CGU
 let erreurConditions = document.getElementById('erreurConditions');
@@ -54,59 +60,72 @@ validation.addEventListener("click", valider);
 function valider(event) {
 
   //validation prénom
-  if (prenom.validity.valueMissing) {
+  if (prenom.value === "") {
     event.preventDefault();
-    erreurPrenom.textContent = " Merci de renseigner votre prénom";
+    erreurPrenom.textContent = "Merci de renseigner votre prénom";
   } else if (prenomValid.test(prenom.value) == false) {
     event.preventDefault();
     erreurPrenom.textContent = " Format du prénom non valide";
   };
 
   //validation nom
-  if (nom.validity.valueMissing) {
+  if (nom.value === "") {
     event.preventDefault();
     erreurNom.textContent = " Merci de renseigner votre nom";
   } else if (nomValid.test(nom.value) == false) {
     event.preventDefault();
-    erreurPrenom.textContent = " Format du nom non valide";
+    erreurNom.textContent = " Format du nom non valide";
   };
 
   //validation email
-  if (email.validity.valueMissing) {
+  if (email.value === "") {
     event.preventDefault();
     erreurMail.textContent = " Merci de renseigner votre mail";
   } else if (emailValid.test(email.value) == false) {
     event.preventDefault();
-    erreurPrenom.textContent = " Format de l'email non valide";
+    erreurMail.textContent = " Format de l'email non valide";
   };
 
   //validation date de naissance
-  if (birthdate.validity.valueMissing) {
+  if (birthdate.value === "") {
     event.preventDefault();
     erreurBirthdate.textContent = " Merci de renseigner votre date de naissance";
   } else if (birthdateValid.test(birthdate.value) == false) {
     event.preventDefault();
-    erreurPrenom.textContent = " jjmmaaaa";
+    erreurBirthdate.textContent = " jjmmaaaa";
   };
 
   //validation nombre de tournois passés
-  if (quantity.validity.valueMissing) {
+  if (quantity.value === "") {
     event.preventDefault();
     erreurQuantity.textContent = " Merci de renseigner le nombre de tournois auquels vous avez participé dans le passé";
   } else if (quantityValid.test(quantity.value) == false) {
     event.preventDefault();
-    erreurPrenom.textContent = "Merci de renseigner un nombre";
+    erreurQuantity.textContent = "Merci de renseigner un nombre";
   };
 
   //validation de la localisation du tournoi
-  if (location.validity.valueMissing) {
+  let lieuChecked = false;
+  for (let i = 0; i < lieu.length; i++) {
+    if (lieu[i].checked) {
+      lieuChecked = true;
+      break;
+    }
+  };
+
+  if (!lieuChecked) {
     event.preventDefault();
-    erreurLocation.textContent = " Merci de cocher le lieu où vous souhaitez participer";
+    erreurLocation.textContent = "Merci de choisir un lieu pour votre inscription au tournois"
   };
 
   //validation CGU
-  if (checkbox1.validity.valueMissing) {
+  if (!checkbox1.checked) {
     event.preventDefault();
     erreurConditions.textContent = " Merci de valider les conditions générales d'utilisation";
   };
-}
+
+  //ouverture de la modal succes
+
+  modalSucces.style.display = "block";
+
+} 
